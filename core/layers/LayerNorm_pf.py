2eff5baa502570ffb0590e203c191a4372306888
aureliayang <yangchen.cug@163.com>
2025-03-25 10:51:36 +0800

    add all

---
+++ core/layers/LayerNorm_pf.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class LayerNorm2D(nn.LayerNorm):
+    def __init__(self, num_channels, eps=1e-6, affine=True):
+        super().__init__(num_channels, eps=eps, elementwise_affine=affine)
+
+    def forward(self, x):
+        return F.layer_norm(
+            x.permute(0, 2, 3, 1), self.normalized_shape, self.weight, self.bias, self.eps
+        ).permute(0, 3, 1, 2)
